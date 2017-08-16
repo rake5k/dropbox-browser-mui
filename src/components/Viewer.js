3c9e2b64b3744105a5538ccee1512110cbfd43b1
Christian Harke <ch.harke@gmail.com>
2017-08-16 23:59:53 +0200

    MS Edge fix

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Viewer(props) {
     const src = `https://docs.google.com/gview?url=${props.file}&embedded=true`;
-    const style = { height: '100%' };
+    const style = { height: '100%', width: '100%' };
 
     return <iframe src={src} style={style} frameBorder="0" title="gview"></iframe>;
 }

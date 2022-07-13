a21f1a0922f8b58ec7af39aeb6bd044fd5fdb356
Christian Harke <christian.harke@bluecare.ch>
2022-07-13 07:02:06 +0200

    Adapt repository mock

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import * as Repository from './repositories/Dropbox';
 import App from './App';
 
-describe('App', () => {
-    beforeEach(() => {
-        Repository.loadEntries = jest.fn(async () => await []);
-        Repository.loadFile = jest.fn(async () => await []);
-    });
+jest.mock('./repositories/Dropbox');
 
+describe('App', () => {
     it('renders without crashing', () => {
         process.env.REACT_APP_TITLE = 'Dropbox Browser test';
         const div = document.createElement('div');

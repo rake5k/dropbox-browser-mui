e31aedc3498c84a59a988863abd284a6556f06f3
Christian Harke <christian.harke@bluecare.ch>
2022-07-16 00:52:50 +0200

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
@@ -8,7 +8,13 @@ jest.mock('./repositories/Dropbox');
 describe('App', () => {
     it('renders without crashing', () => {
         process.env.REACT_APP_TITLE = 'Dropbox Browser test';
-        const div = document.createElement('div');
-        ReactDOM.render(<App />, div);
+        const root = ReactDOM.createRoot(
+            document.createElement('root') as HTMLElement,
+        );
+        root.render(
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>,
+        );
     });
 });

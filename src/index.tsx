017030227a7c662de3cd11b9f39c12d63430eba5
Christian Harke <christian.harke@bluecare.ch>
2022-07-16 00:49:17 +0200

    Use react's strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,11 @@ import 'index.css';
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

67fcaba8ae7f0047de56168f1a3ecbf3933e12e4
Christian Harke <christian.harke@bluecare.ch>
2022-07-19 21:48:56 +0200

    Update service worker implementation since not working in preview apps

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from 'App';
-import * as serviceWorker from 'serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import 'index.css';
 
@@ -17,5 +17,5 @@ root.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();

281c92ffb486bbac5c6b5485b4343a821dee370f
Christian Harke <christian.harke@bluecare.ch>
2022-07-20 00:48:11 +0200

    Make log level configurable via `REACT_APP_LOG_LEVEL`

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,8 +1,65 @@
-import log from 'loglevel';
+import _ from 'lodash';
+import log, { LogLevelNumbers } from 'loglevel';
+
+function isNum(level: string) {
+    return !_.isNaN(level);
+}
+
+const convertLogLevel = (
+    level: string | undefined,
+): LogLevelNumbers | undefined => {
+    if (!level) {
+        log.debug(`No specific log level provided, will set the default one.`);
+        return;
+    }
+
+    if (isNum(level) && _.toNumber(level) <= log.levels.SILENT) {
+        return _.toNumber(level) as LogLevelNumbers;
+    }
+
+    switch (level.toUpperCase()) {
+        case 'TRACE':
+            return log.levels.TRACE;
+        case 'DEBUG':
+            return log.levels.DEBUG;
+        case 'INFO':
+            return log.levels.INFO;
+        case 'WARN':
+            return log.levels.WARN;
+        case 'ERROR':
+            return log.levels.ERROR;
+        case 'SILENT':
+            return log.levels.SILENT;
+        default:
+            throw new Error(`Invalid log level \`${level}\` provided!`);
+    }
+};
+
+const getLogLevelName = (level: LogLevelNumbers): string => {
+    switch (level) {
+        case log.levels.TRACE:
+            return 'TRACE';
+        case log.levels.DEBUG:
+            return 'DEBUG';
+        case log.levels.INFO:
+            return 'INFO';
+        case log.levels.WARN:
+            return 'WARN';
+        case log.levels.ERROR:
+            return 'ERROR';
+        case log.levels.SILENT:
+            return 'SILENT';
+        default:
+            throw new Error(`Invalid log level number \`${level}\` provided!`);
+    }
+};
 
 const isProduction = process.env.NODE_ENV === 'production';
-const level = isProduction ? log.levels.WARN : log.levels.TRACE;
-log.setLevel(level);
+const defaultLevel = isProduction ? log.levels.WARN : log.levels.TRACE;
+const selectedLevel =
+    convertLogLevel(process.env.REACT_APP_LOG_LEVEL) || defaultLevel;
+log.setLevel(selectedLevel);
+log.info(`Log level set to \`${getLogLevelName(log.getLevel())}\`.`);
 
 export const trace = (msg: string, ...args: any[]) => {
     log.trace(msg, args);

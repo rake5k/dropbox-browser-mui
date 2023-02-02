77b525a721340be1cd9c0c621d4cefa2bffea665
Christian Harke <christian.harke@bluecare.ch>
2023-02-02 22:43:56 +0100

    Add prettier autoformat pre-commit hook

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
-  readonly VITE_DROPBOX_ACCESS_TOKEN: string
-  readonly VITE_LOG_LEVEL: string | number
+    readonly VITE_APP_TITLE: string;
+    readonly VITE_DROPBOX_ACCESS_TOKEN: string;
+    readonly VITE_LOG_LEVEL: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+    readonly env: ImportMetaEnv;
 }
-

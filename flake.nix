e3449e922c442ee3a8d1026f7eb2b28154b28f4a
Christian Harke <christian.harke@bluecare.ch>
2023-02-09 21:57:28 +0100

    Add arm architecture to supported systems

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
       name = "dropbox-browser-mui";
 
       # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
+      supportedSystems = [ "aarch64-linux" "x86_64-linux" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
